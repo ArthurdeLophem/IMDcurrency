3b8eae341d01d420947f35e54145e01e0b11a41c
r0808 <arthurdelophem3@gmail.com>
2022-05-12 12:17:03 +0200

    feat: test for broken routes

--- js/register.js
+++ js/register.js
@@ -2,7 +2,7 @@ document.querySelector(".btn").addEventListener("click", (e) => {
     let username = document.querySelector(".username").value;
     let email = document.querySelector(".email").value;
     let password = document.querySelector(".password").value;
-
+    console.log(username, email, password)
     fetch('http://localhost:3001/users/api/v1/signup/', {
         method: 'POST',
         headers: {
@@ -14,8 +14,10 @@ document.querySelector(".btn").addEventListener("click", (e) => {
             "password": password
         })
     }).then(result => {
+        console.log("hallo")
         console.log(result.json())
     }).catch((error) => {
+        console.log("fout")
         console.log(error.message);
     });
 })
