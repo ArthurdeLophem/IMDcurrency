71f577396bfe4cdca427a0a29903d76c88b90368
r0808 <arthurdelophem3@gmail.com>
2022-05-29 19:17:08 +0200

    feat: user can't login without student mail

--- js/register.js
+++ js/register.js
@@ -1,3 +1,5 @@
+let emailChecked;
+
 document.querySelector(".btn").addEventListener("click", (e) => {
     let username = document.querySelector(".username").value;
     let email = document.querySelector(".email").value;
@@ -10,36 +12,38 @@ document.querySelector(".btn").addEventListener("click", (e) => {
         "password": password
     }
     console.log(data);
-    fetch(baseUrl + 'api/v1/users/signup', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data),
-    }).then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            if (data.success == true) {
-                console.log(data.message);
-                let userData =
-                {
-                    "_id": data.message._id,
-                    "coins": data.message.coins,
-                    "username": data.message.username,
-                    "token": data.message.token,
-                }
-                localStorage.setItem("userData", JSON.stringify(userData));
-                window.location.href = "index.html";
-            }
+    if (emailChecked) {
+        fetch(baseUrl + 'api/v1/users/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
         })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                if (data.success == true) {
+                    console.log(data.message);
+                    let userData =
+                    {
+                        "_id": data.message._id,
+                        "coins": data.message.coins,
+                        "username": data.message.username,
+                        "token": data.message.token,
+                    }
+                    localStorage.setItem("userData", JSON.stringify(userData));
+                    window.location.href = "index.html";
+                }
+            })
 
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
 })
 
 document.querySelector('#email').addEventListener('change', (e) => {
-    let emailChecked;
     if (e.target.value.includes('@student.thomasmore.be')) {
         e.target.classList.remove('form__input--invalid')
         emailChecked = true;
