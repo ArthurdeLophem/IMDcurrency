a66e00bcebe276103f14179c1c15f82e31d494cc
r0808 <arthurdelophem3@gmail.com>
2022-05-29 17:48:44 +0200

    feat: add feedback/validation on create new transaction

--- js/transaction.js
+++ js/transaction.js
@@ -24,24 +24,68 @@ document.querySelector("#submitTransaction").addEventListener("click", (e) => {
     e.preventDefault();
     const amount = document.querySelector("#amount").value;
     const message = document.querySelector("#message").value;
-    console.log(amount)
-    fetch(baseUrl + '/api/v1/transfers/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "message": message,
-            "_id": localStorage.getItem('_uid'),
-            "to_user": toUserId,
-            "amount": amount,
-            "completed": true
-        })
-    }).then(res => res.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch((error) => {
-            console.log(error.message);
-        });
+
+    if (message == null || message == "", amount == null || amount == "") {
+        document.querySelector('form').style.display = 'none';
+
+        let container = document.createElement('div');
+        container.classList.add('container');
+        container.style.alignItems = 'center';
+
+        let feedback = document.createElement('img');
+        let feedbackText = document.createElement('p');
+
+        feedbackText.innerHTML = 'transfer failed, try again';
+        feedback.src = './images/failed.svg';
+        feedback.classList.add('form__feedback--failed');
+        container.appendChild(feedback);
+        container.appendChild(feedbackText);
+        document.querySelector('body').appendChild(container);
+
+        window.setTimeout(() => {
+            window.location.href = "transaction.html";
+        }, 2000);
+    } else {
+        fetch(baseUrl + '/api/v1/transfers/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "message": message,
+                "_id": localStorage.getItem('_uid'),
+                "to_user": toUserId,
+                "amount": amount,
+                "completed": true
+            })
+        }).then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.status === 'success') {
+                    document.querySelector('form').style.display = 'none';
+
+                    let container = document.createElement('div');
+                    container.classList.add('container');
+                    container.style.alignItems = 'center';
+
+                    let feedback = document.createElement('img');
+                    let feedbackText = document.createElement('p');
+
+                    feedbackText.innerHTML = 'transfer success, redirecting back home';
+                    feedback.src = './images/success.svg';
+                    feedback.classList.add('form__feedback--success');
+                    container.appendChild(feedback);
+                    container.appendChild(feedbackText);
+                    document.querySelector('body').appendChild(container);
+
+                    window.setTimeout(() => {
+                        window.location.href = "index.html";
+                    }, 2000);
+                }
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    }
+
 })
